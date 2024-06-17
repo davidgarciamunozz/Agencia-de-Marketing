08d89cde7a8e054e68565cef8a1d4818149957dc
davidgarciamunozz <garxia0710@gmail.com>
2024-06-17 17:17:02 -0500

    lastMod

--- src/containers/pages/About.jsx
+++ src/containers/pages/About.jsx
@@ -3,6 +3,13 @@ import Layout from "../../hocs/layouts/Layout";
 import Footer from "../../components/navigation/Footer";
 import { useEffect } from "react";
 import Header from "components/About/Header";
+import TestStats from "components/About/TestStats";
+import Images from "components/About/Images";
+import OurClients from "components/About/OurClients";
+import LogoCloud from "components/Home/LogoCloud";
+import Features from "components/About/Features";
+import OurTeam from "components/About/Team";
+import CTA from "components/About/CTA";
 
 function About () {
   useEffect(() => {
@@ -13,6 +20,13 @@ function About () {
       <Navbar />
       <div className="pt-28">
         <Header />
+        <TestStats />
+        <Images />
+        <OurClients />
+        <LogoCloud />
+        <Features />
+        <OurTeam />
+        <CTA />
       </div>
       <Footer />
     </Layout>
